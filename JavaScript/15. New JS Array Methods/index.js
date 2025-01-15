5747249f54ddb37300f68bb46a6356f012964e8a
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-01-15 04:34:09 +0000

    15. New JS Array Meethods

---
+++ JavaScript/15. New JS Array Methods/index.js
@@ -0,0 +1,21 @@
+// https://www.youtube.com/watch?v=36EsJsju8r4
+
+// The three methods:
+// toSorted()
+// toReversed()
+// toSpliced() 
+
+
+const myArray = ["Jhon", "Wick", "Maria", "Albert", "Andrew"];
+
+// console.log(myArray.sort())
+console.log(myArray.toSorted())
+console.log(myArray);
+
+// console.log(myArray.reverse())
+console.log(myArray.toReversed())
+console.log(myArray);
+
+// console.log(myArray.splice(2))
+console.log(myArray.toSpliced(2))
+console.log(myArray);
