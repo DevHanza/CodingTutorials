99d03882667aa2533e742c951d1efa4da41cdaf9
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-02-23 05:09:59 +0000

    Learn React With This ONE Project (Tech WIth Tim) - [First ever React course i've took.]

---
+++ React/Learn React With This ONE Project/frontend/src/services/api.js
@@ -0,0 +1,22 @@
+// API Website: https://www.themoviedb.org/
+
+const API_KEY = "72fab7d489997f1e3cafbb23fa0da59b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+
+  const data = await response.json();
+  return data.results;
+};
