695107e5771efed8ed299beb62c4f679ee1e6f6a
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-02-26 03:05:53 +0000

    Angular: Use SVG as templates

---
+++ Angular/46. use the SVG as templates/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = '46. use the SVG as templates';
+
+  fillColor = 'red';
+
+  changeColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    this.fillColor = `rgb(${r}, ${g}, ${b})`;
+  }
+}
