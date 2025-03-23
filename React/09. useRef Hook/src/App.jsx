29d551dd0c13a59f6cd123aedaaa6f73a4b502a3
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-03-23 15:51:35 +0000

    useRef Hook : Part 1

---
+++ React/09. useRef Hook/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FocusInput from './components/FocusInput'
+
+function App() {
+  return (
+    <div>
+      <FocusInput></FocusInput>
+    </div>
+  )
+}
+
+export default App
