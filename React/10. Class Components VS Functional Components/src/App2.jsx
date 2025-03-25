fd267ec7444fbbb978435575213f950e6de41443
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-03-25 17:10:58 +0000

    React Class Comp. vs Func Components

---
+++ React/10. Class Components VS Functional Components/src/App2.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class App2 extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            name: "",
+            age: 100,
+            isMale: true,
+        };
+    }
+
+  render() {
+    const { name, age, isMale } = this.state; 
+
+    return (
+      <div>
+        <h1>My name is {name}</h1>
+        <h2>I am {age} years old.</h2>
+        <h3>I am a {isMale ? "Male" : "Female"}</h3>
+
+        <h4>I am feeling {this.props.emotion}</h4>
+      </div>
+    )
+  }
+}
+
+export default App2;
