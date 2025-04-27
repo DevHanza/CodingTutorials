e27bc45c9699054c7bfd527c40d7e5c30f5682ae
DevHanza <75363593+DevHanza@users.noreply.github.com>
2025-04-27 15:16:49 +0000

    React with TypeScript:Types for Props &  Reusable Types

--- React/14. THE BIGGEST REACT.JS COURSE ON THE INTERNET/Part 1/React With TypeScript/Exercise/src/App.tsx
+++ React/14. THE BIGGEST REACT.JS COURSE ON THE INTERNET/Part 1/React With TypeScript/Exercise/src/App.tsx
@@ -1,19 +1,20 @@
-import Button from "./Button";
+import Button from "./components/Button";
 import UserInfo from "./components/UserInfo";
 import AdminInfo from "./components/AdminInfo";
+import { Info, AdminInfoList } from "./types";
 
 function App() {
   function handleClick() {
     console.log("Handle is Clicked");
   }
 
-  const user = {
+  const user: Info = {
     id: 1,
     name: "Jhon Doe",
     email: "jhon@gmail.com",
   };
 
-  const admin = {
+  const admin: AdminInfoList = {
     id: 2,
     name: "Jane Smith",
     email: "jane@gmail.com",
@@ -26,8 +27,8 @@ function App() {
       <UserInfo user={user} />
       <AdminInfo admin={admin} />
 
-      <Button label="Click Me" onClick={() => handleClick} disabled={false} />
-      <Button label="Click Me" onClick={() => handleClick} disabled={true} />
+      <Button label="Click Me" onClick={() => handleClick()} disabled={false} />
+      <Button label="Click Me" onClick={() => handleClick()} disabled={true} />
     </div>
   );
 }
--- React/14. THE BIGGEST REACT.JS COURSE ON THE INTERNET/Part 1/React With TypeScript/Exercise/src/components/UserInfo.tsx
+++ React/14. THE BIGGEST REACT.JS COURSE ON THE INTERNET/Part 1/React With TypeScript/Exercise/src/components/UserInfo.tsx
@@ -1,5 +1,18 @@
-function UserInfo() {
-  return <div></div>;
+import { Info } from "../types";
+
+type UserInfoProp = {
+  user: Info;
+};
+
+function UserInfo({ user }: UserInfoProp) {
+  return (
+    <div>
+      <h2>User Information</h2>
+      <p>ID: {user.id}</p>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
 }
 
 export default UserInfo;
