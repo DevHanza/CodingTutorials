26669b7dca5a0b0b3c1b62916d2246e1e748b8c9
Hansana Prabath <75363593+DevHanza@users.noreply.github.com>
2025-05-03 11:18:37 +0000

    Types for useEffect: React + TS

---
+++ React/14. THE BIGGEST REACT.JS COURSE ON THE INTERNET/Part 1/React With TypeScript/Types for useReducer/Tutorial/src/App.tsx
@@ -0,0 +1,13 @@
+import Counter from "./components/Counter";
+import CounterReducter from "./components/CounterReducter";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <CounterReducter />
+    </div>
+  );
+}
+
+export default App;
