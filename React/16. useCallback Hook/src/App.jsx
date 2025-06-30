76b09818645505ecffa1b469e0ffa99d00c4658e
DevHanza <hansana123dasanayake@gmail.com>
2025-06-30 21:14:33 +0530

    16. useCallback Hook (After)

--- React/16. useCallback Hook/src/App.jsx
+++ React/16. useCallback Hook/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import List from "./List";
 
 function App() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  const getItems = () => {
+  const getItems = useCallback(() => {
     return [number, number + 1, number + 2];
-  };
+  }, [number]);
 
   const theme = {
     backgroundColor: dark ? "#333" : "#FFF",
