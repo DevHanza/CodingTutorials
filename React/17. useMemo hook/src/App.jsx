3e99a938df47473ba14f1e2486e17a3ae6048797
DevHanza <hansana123dasanayake@gmail.com>
2025-07-01 21:57:24 +0530

    useMemo - After

--- React/17. useMemo hook/src/App.jsx
+++ React/17. useMemo hook/src/App.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function App() {
   const [number, setNumber] = useState(0);
   const [dark, setDark] = useState(false);
 
-  const doubleNumber = slowFunction(number);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
 
   const themeStyles = {
     backgroundColor: dark ? "black" : "white",
