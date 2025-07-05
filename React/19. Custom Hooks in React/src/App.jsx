18661538e22c01be2acd59e671d17a878f167d12
DevHanza <hansana123dasanayake@gmail.com>
2025-07-05 21:17:58 +0530

    19. Custom Hooks in React

---
+++ React/19. Custom Hooks in React/src/App.jsx
@@ -0,0 +1,31 @@
+import { useOnlineStatus } from "./useOnlineStatus.jsx";
+
+function App() {
+  return (
+    <div>
+      <StatusBar />
+      <SaveButton />
+    </div>
+  );
+}
+export default App;
+
+function StatusBar() {
+  const isOnline = useOnlineStatus();
+  return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
+}
+
+function SaveButton() {
+  const isOnline = useOnlineStatus();
+
+  function handleSaveClick() {
+    console.log("✅ Progress saved");
+  }
+
+  return (
+    <button disabled={!isOnline} onClick={handleSaveClick}>
+      {isOnline ? "Save progress" : "Reconnecting..."}
+    </button>
+  );
+}
+
