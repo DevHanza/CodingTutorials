c7cb379ae6f711ee5b04996854c2cad4d18eec02
DevHanza <hansana123dasanayake@gmail.com>
2025-07-24 22:07:22 +0530

    useDeferredValue in React

---
+++ React/22. useDeferredValue Hook/src/SlowList.jsx
@@ -0,0 +1,20 @@
+import { memo } from "react";
+
+const SlowList = memo(({ text }) => {
+  const items = [];
+  for (let i = 0; i < 250; i++) {
+    items.push(<SlowItem key={i} text={text} />);
+  }
+  return <ul className="items">{items}</ul>;
+});
+
+const SlowItem = ({ text }) => {
+  let startTime = performance.now();
+  while (performance.now() - startTime < 1) {
+    // Do nothing for 1 ms per item to emulate extremely slow code
+  }
+
+  return <li className="item">Text: {text}</li>;
+};
+
+export default SlowList;
